f6f69d11ad70c8d381d984fa7707007cbb4123cd
Louis Latour <louis.latour@philips.com>
2021-01-04 22:11:38 +0100

    adding loop and cuda

--- regex/play.sh
+++ regex/play.sh
@@ -8,7 +8,7 @@ fi
 
 # running playbook
 echo executing: ansible-playbook -i inventory.ini $PLAYBOOK
-ansible-playbook -i inventory.ini $PLAYBOOK 
+ansible-playbook -i inventory.ini $PLAYBOOK -e global_regcred_api_key=$AF_KEY
 rc=$?
  
 exit $rc
